3ad7bef2e96a23b268588819203e173d52bd1327
Pratim-21 <pandey.pratim21>
2022-04-05 15:26:39 +0530

    test comment

--- src/main/java/io/javabrains/CourseApiDataApplication.java
+++ src/main/java/io/javabrains/CourseApiDataApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApiDataApplication {
 
-	//test comment
+	//test comment to check commit
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiDataApplication.class, args);
 	}
